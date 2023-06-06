250117fe9a16d97f76c0e8b9ed5fcbba8d9b2589
The Man <msblauwal6@gmail.com>
2023-06-06 17:25:21 +0200

    Add foundation for rancher mgmt node provisioning

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+provider "vault" {
+  address = "http://10.0.1.152:8200"
+  #token = VAULT_TOKEN ENV VAR
+}
+
 data "vault_generic_secret" "pm_api_token_id" {
   path = "secret/proxmox/api_token_id"
 }
@@ -16,8 +21,31 @@ data "vault_generic_secret" "pm_api_token_secret" {
 }
 
 provider "proxmox" {
-    pm_tls_insecure = true
-    pm_api_url = "https://pvx.host:8006/api2/json"
-    pm_api_token_id = data.vault_generic_secret.pm_api_token_id["value"]
-    pm_api_token_secret = data.vault_generic_secret.pm_api_token_secret["value"]
+  pm_tls_insecure     = true
+  pm_api_url          = "https://10.0.1.10:8006/api2/json"
+  pm_api_token_id     = data.vault_generic_secret.pm_api_token_id.data["value"]
+  pm_api_token_secret = data.vault_generic_secret.pm_api_token_secret.data["value"]
+}
+
+// Reference the modules/rancher-mgmt module
+module "rancher_mgmt" {
+  source = "./modules/rancher-mgmt"
+
+  cluster_name = "rancher"
+  cluster_fqdn = "rancher.klofpi.net"
+
+  rancher_master_nodes = [
+    {
+      name = "rancher-master-01"
+      ip   = 2
+    },
+    {
+      name = "rancher-master-02"
+      ip   = 3
+    },
+    {
+      name = "rancher-master-03"
+      ip   = 4
+    }
+  ]
 }
---
+++ modules/rancher-mgmt/variables.tf
@@ -0,0 +1,34 @@
+variable "cluster_name" {
+  description = "The name to use for the cluster."
+  type        = string
+}
+
+variable "cluster_fqdn" {
+  description = "The FQDN that the cluster will use."
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}$", var.cluster_fqdn))
+    error_message = "The cluster FQDN must be a valid domain name."
+  }
+}
+
+variable "rancher_master_nodes" {
+  description = "Master node configuration set."
+  type = list(object({
+    name = string,
+    ip   = number
+  }))
+
+  // Validate that name is unique
+  validation {
+    condition     = length(var.rancher_master_nodes) == length(distinct([for n in var.rancher_master_nodes : n.name]))
+    error_message = "The master node name must be unique."
+  }
+
+  // Validate that ip is a number between 2 and 25
+  validation {
+    condition     = alltrue([for n in var.rancher_master_nodes : n.ip > 1 && n.ip < 26])
+    error_message = "The master node IP must be a number between 2 and 25."
+  }
+}
