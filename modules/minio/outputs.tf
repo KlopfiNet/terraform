c113f105487bf78af830c9541fa691bf837ccbb3
ThisIsntTheWay <msblauwal6@gmail.com>
2024-01-02 12:06:12 +0000

    Create velero user SA

--- modules/minio/outputs.tf
+++ modules/minio/outputs.tf
@@ -1,4 +1,9 @@
-output "velero_user_secret" {
-  value = "${minio_iam_user.velero_user.secret}"
+output "velero_user_sa_key" {
+  value     = minio_iam_service_account.velero_service_account.access_key
+  sensitive = false
+}
+
+output "velero_user_sa_secret" {
+  value     = minio_iam_service_account.velero_service_account.secret_key
   sensitive = true
 }
--- modules/minio/users.tf
+++ modules/minio/users.tf
@@ -6,7 +6,19 @@ resource "minio_iam_user" "velero_user" {
   }
 }
 
-resource "minio_iam_user_policy_attachment" "velero_policy" {
+resource "minio_iam_user_policy_attachment" "velero" {
   user_name   = minio_iam_user.velero_user.id
   policy_name = minio_iam_policy.velero_policy.id
+}
+
+resource "minio_iam_service_account" "velero_service_account" {
+  target_user = minio_iam_user.velero_user.name
+
+  # Not specifying this lets the SA inherit the user policy
+  # However, the dumdum provider assumes that you explicitly provide a policy here
+  #policy      = minio_iam_policy.velero_policy.policy
+
+  lifecycle {
+    ignore_changes = [policy]
+  }
 }
