a35d2ce592ec1a5e8db94ab76f5f42e6efeea22a
ThisIsntTheWay <msblauwal6@gmail.com>
2024-01-02 12:06:23 +0000

    Fix S3 policy

--- modules/minio/policies.tf
+++ modules/minio/policies.tf
@@ -5,15 +5,23 @@ resource "minio_iam_policy" "velero_policy" {
         "Version":"2012-10-17",
         "Statement": [
             {
-            "Sid":"ListAllBucket",
-            "Effect": "Allow",
-            "Action": [
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:DeleteObject"
-            ],
-            "Principal":"*",
-            "Resource": "arn:aws:s3:::${minio_s3_bucket.velero_bucket.bucket}/*"
+                "Effect": "Allow",
+                "Action": [
+                    "s3:ListBucket",
+                    "s3:ListAllMyBuckets",
+                    "s3:GetBucketLocation"
+                ],
+                "Resource": "arn:aws:s3:::${minio_s3_bucket.velero_bucket.bucket}"
+            },
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "s3:PutObject",
+                    "s3:GetObject",
+                    "s3:GetObjectVersion",
+                    "s3:DeleteObject"
+                ],
+                "Resource": "arn:aws:s3:::${minio_s3_bucket.velero_bucket.bucket}/*"
             }
         ]
     }
