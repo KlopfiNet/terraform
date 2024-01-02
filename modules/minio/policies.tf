7542e9cf1ff8fa312fccb1cc308811fcd0fb67f9
ThisIsntTheWay <msblauwal6@gmail.com>
2024-01-02 13:37:45 +0000

    Adjust policy

--- modules/minio/policies.tf
+++ modules/minio/policies.tf
@@ -9,17 +9,19 @@ resource "minio_iam_policy" "velero_policy" {
                 "Action": [
                     "s3:ListBucket",
                     "s3:ListAllMyBuckets",
-                    "s3:GetBucketLocation"
+                    "s3:GetBucketLocation",
+                    "s3:GetBucketObjectLockConfiguration"
                 ],
                 "Resource": "arn:aws:s3:::${minio_s3_bucket.velero_bucket.bucket}"
             },
             {
                 "Effect": "Allow",
                 "Action": [
-                    "s3:PutObject",
                     "s3:GetObject",
-                    "s3:GetObjectVersion",
-                    "s3:DeleteObject"
+                    "s3:DeleteObject",
+                    "s3:PutObject",
+                    "s3:AbortMultipartUpload",
+                    "s3:ListMultipartUploadParts"
                 ],
                 "Resource": "arn:aws:s3:::${minio_s3_bucket.velero_bucket.bucket}/*"
             }
