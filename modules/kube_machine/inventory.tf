7736141386e82ad792c59c8ae7eb7780ce33f661
ThisIsntTheWay <msblauwal6@gmail.com>
2024-01-12 10:30:00 +0000

    Make "inventory" an output

--- modules/kube_machine/inventory.tf
+++
@@ -1,8 +0,0 @@
-# Create ansible inventory file
-resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/templates/inventory.tmpl", {
-    hosts      = var.nodes
-    ip_network = local.ip_network
-  })
-  filename = "./outputs/inventory_kubernetes.ini"
-}
--- modules/load_balancer/outputs.tf
+++ modules/load_balancer/outputs.tf
@@ -1,3 +1,9 @@
 output "vm_password" {
   value = random_password.vm_password.result
 }
+
+output "inventory" {
+  value = templatefile("${path.module}/templates/inventory.tmpl", {
+    ip = var.vm_ip_address
+  })
+}
