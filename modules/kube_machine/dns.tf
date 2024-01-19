fa2ca83f7a963a1e950f51cf006f8aa6ba3ea73a
ThisIsntTheWay <msblauwal6@gmail.com>
2024-01-19 21:34:53 +0000

    Add PTR records

--- modules/kube_machine/dns.tf
+++ modules/kube_machine/dns.tf
@@ -4,6 +4,10 @@ provider "dns" {
   }
 }
 
+locals {
+  reverse_zone_ip = join(".", reverse(split(".", local.ip_network)))
+}
+
 resource "dns_a_record_set" "kube_machine" {
   for_each = { for n in var.nodes : n.name => n }
 
@@ -12,5 +16,14 @@ resource "dns_a_record_set" "kube_machine" {
   addresses = [
     "${local.ip_network}.${each.value.ip_octet}",
   ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "kube_machine" {
+  for_each = { for n in var.nodes : n.name => n }
+
+  zone = "${local.reverse_zone_ip}.in-addr.arpa."
+  name = each.value.ip_octet
+  ptr = "${each.value.name}.${local.dns_zone}."
   ttl = 300
 }
